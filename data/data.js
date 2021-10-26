d69ffd5322763d3c714a83dd2fd0927527a05a88
Александр Ковтун <Maldades@mail.ru>
2021-10-26 12:24:37 +0300

    sketch of the first location

--- data/data.js
+++ data/data.js
@@ -1,10 +1,10 @@
 var DATA = {
 	gravity: 10,
-	windowWidth: 1000,
-	windowHeight: 1000,
+	windowWidth: 2000,
+	windowHeight: 800,
 	mainHero: {
-		x: 100,
-		y: 230,
+		x: 20,
+		y: 745,
 		w: 30,
 		h: 40,
 		jumpHeight: 10,
@@ -21,60 +21,117 @@ var DATA = {
 	roomOne: {
 		obstacles: [
 			{
+				type: "exit",
+				x: 2,
+				y: 745,
+				w: 7,
+				h: 50,
+				color: "#8b00ff"
+			},
+			{
 				type: "platform1",
 				x: 0,
-				y: 300,
+				y: 685,
 				w: 150,
 				h: 5,
 				color: "#964b00"
 			},
 			{
 				type: "platform2",
-				x: 250,
-				y: 400,
-				w: 150,
+				x: 0,
+				y: 620,
+				w: 100,
 				h: 5,
 				color: "#964b00"
 			},
 			{
 				type: "platform3",
-				x: 0,
-				y: 495,
-				w: 700,
-				h: 5,
-				color: "#964b00"
-			},
-			{
-				type: "platform4",
-				x: 800,
-				y: 600,
-				w: 700,
-				h: 5,
-				color: "#964b00"
-			},
-			{
-				type: "platform5",
-				x: 0,
-				y: 1000,
-				w: 1500,
-				h: 5,
-				color: "#964b00"
-			},
-			{
-				type: "platform3",
-				x: 555,
-				y: 495,
+				x: 585,
+				y: 650,
 				w: 495,
 				h: 5,
 				color: "#964b00"
 			},
 			{
-				type: "box",
-				x: 250,
-				y: 230,
+				type: "platform4",
+				x: 750,
+				y: 550,
+				w: 200,
+				h: 5,
+				color: "#964b00"
+			},
+			{
+				type: "platform5",
+				x: 1180,
+				y: 450,
+				w: 100,
+				h: 5,
+				color: "#964b00"
+			},
+			{
+				type: "platform6",
+				x: 555,
+				y: 450,
 				w: 50,
+				h: 5,
+				color: "#964b00"
+			},
+			{
+				type: "platform7",
+				x: 300,
+				y: 150,
+				w: 250,
+				h: 5,
+				color: "#964b00"
+			},
+			{
+				type: "partition1",
+				x: 550,
+				y: 0,
+				w: 5,
+				h: 550,
+				color: "#964b00"
+			},
+			{
+				type: "teleport1",
+				x: 2,
+				y: 585,
+				w: 30,
+				h: 30,
+				img:"",
+				color: "#AFEEEE"
+			},
+			{
+				type: "teleport2",
+				x: 330,
+				y: 120,
+				w: 30,
+				h: 30,
+				color: "#AFEEEE"
+			},
+			{
+				type: "box1",
+				x: 350,
+				y: 715,
+				w: 150,
 				h: 80,
-				color: "#7fffd4"
+				color: "#964b00"
+			},
+			{
+				type: "box2",
+				x: 950,
+				y: 500,
+				w: 150,
+				h: 300,
+				color: "#964b00"
+			},
+			{
+				type: "floor",
+				x: 0,
+				y: 795,
+				w: 2000,
+				h: 5,
+				color: "#964b00"
 			},
 			{
 				type: "wall",
