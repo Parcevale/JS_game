54707d82ed8d29e4e77fcd7782e730ff02242d2c
Nikolay <lgumoscow@gmail.com>
2021-10-26 08:19:01 +0300

    add movement and gravity. jump bagged

---
+++ data/data.js
@@ -0,0 +1,63 @@
+var DATA = {
+	gravity: 4,
+	mainHero: {
+		x: 100,
+		y: 230,
+		w: 30,
+		h: 40,
+		jumpHeight: 10,
+		jumpSpeed: 10,
+		speed: 5,
+		color :"#0979db",
+		actions: {}
+	},
+	actions: [
+		{name : "moveRight", key: "KeyD"},
+		{name : "moveLeft", key: "KeyA"},
+		{name : "moveUp", key: "KeyW"}
+	],
+	roomOne: {
+		obstacles: [
+			{
+				type: "platform1",
+				x: 0,
+				y: 300,
+				w: 150,
+				h: 5,
+				color: "#964b00"
+			},
+			{
+				type: "platform2",
+				x: 250,
+				y: 400,
+				w: 150,
+				h: 5,
+				color: "#964b00"
+			},
+			{
+				type: "platform3",
+				x: 0,
+				y: 495,
+				w: 495,
+				h: 5,
+				color: "#964b00"
+			},
+			{
+				type: "box",
+				x: 250,
+				y: 230,
+				w: 50,
+				h: 80,
+				color: "#7fffd4"
+			},
+			{
+				type: "wall",
+				x: 0,
+				y: 0,
+				w: 5,
+				h: 300,
+				color: "#7fffd4"
+			}
+		]
+	}
+}
--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var aUnderfoot = [];
 var keyUp = false, keyDown = false, keyLeft = false, keyRight = false;
 var gravity;
 var bounce;
+// var mydata = require('./data/data.json')
+// require('./data.json')
+console.log('json data', DATA);
 
 var clear = function() {
 	ctx.fillStyle = "#E3E3E3";
@@ -18,40 +21,38 @@ var draw = function(x,y,w,h,color) {
 } 
 
 function onStartGame() {
-	gravity = setInterval(fGravity, 1000 / 60);
+	// gravity = setInterval(fGravity, 1000 / 60);
 }
 
-function fBounce() {
+// function fBounce() {
 	
-	this.mainObj.y = this.mainObj.y - 120;
-	this.onStartGame();
-}
+// 	DATA.mainHero.y = DATA.mainHero.y - 120;
+// 	this.onStartGame();
+// }
 
-function fGravity(e) {
-	if (aUnderfoot[0].y - 40 == this.mainObj.y && this.mainObj.x <= aUnderfoot[0].w || this.mainObj.y >= 455) {
-		clearInterval(gravity);
-	} else {
-		this.mainObj.y = this.mainObj.y + 4;
-	}
-	if (aUnderfoot[1].y - 40 == this.mainObj.y && this.mainObj.x >= aUnderfoot[1].x || this.mainObj.y >= 455) {
-		clearInterval(gravity);
-	}
-	if (aUnderfoot[0].w -2 == this.mainObj.x) {
-		this.onStartGame();
-    }
-}
+// function fGravity(e) {
+// 	if (aUnderfoot[0].y - 40 == DATA.mainHero.y && DATA.mainHero.x <= aUnderfoot[0].w || DATA.mainHero.y >= 455) {
+// 		clearInterval(gravity);
+// 	} else {
+// 		DATA.mainHero.y = DATA.mainHero.y + 4;
+// 	}
+// 	if (aUnderfoot[1].y - 40 == DATA.mainHero.y && DATA.mainHero.x >= aUnderfoot[1].x || DATA.mainHero.y >= 455) {
+// 		clearInterval(gravity);
+// 	}
+// 	if (aUnderfoot[0].w -2 == DATA.mainHero.x) {
+// 		this.onStartGame();
+//     }
+// }
 
-function onButton() {
-	console.log('123');
-	
-	aObjects.push({
-	x: 10,
-	y: 20,
-	w: 30,
-	h: 40,
-	color :"#ff7f50"
-});
-}
+// function onButton() {
+// 	aObjects.push({
+// 	x: 10,
+// 	y: 20,
+// 	w: 30,
+// 	h: 40,
+// 	color :"#ff7f50"
+// });
+// }
 // var drawEl = function() {
 // 	drawMain(20,20,10,10,'#DD5656');
 // }
@@ -59,60 +60,140 @@ clear();
 draw(20,20,10,10,'#DD5656');
 var mainLoop = function() {
 	clear();
+	aObjects.forEach(calcObjects);
 	aObjects.forEach(function(obj){
 		draw(obj.x,obj.y,obj.w,obj.h,obj.color);
 	})
-	aUnderfoot.forEach(function (obj) {
+	DATA.roomOne.obstacles.forEach(function (obj) {
 		draw(obj.x, obj.y, obj.w, obj.h, obj.color);
 	})
-	console.log('refresh');
+	// console.log('refresh');
 }
-this.mainObj = {
-	x: 10,
-	y: 20,
-	w: 30,
-	h: 40,
-	
-	color :"#0979db"
-};
-this.underfootOne = {
-	x: 0,
-	y: 300,
-	w: 150,
-	h: 5,
-	color: "#964b00"
-};
-this.underfootTwo = {
-	x: 250,
-	y: 400,
-	w: 150,
-	h: 5,
-	color: "#964b00"
-};
-this.underfoot = {
-	x: 0,
-	y: 495,
-	w: 495,
-	h: 5,
-	color: "#964b00"
-};
-aObjects.push(this.mainObj);
-aUnderfoot.push(this.underfootOne);
-aUnderfoot.push(this.underfootTwo);
-aUnderfoot.push(this.underfoot);
+aObjects.push(DATA.mainHero);
+// aUnderfoot.push(this.underfootOne);
+// aUnderfoot.push(this.underfootTwo);
 
+// aUnderfoot.push(this.underfoot);
+//скомпоновать keyup и keydown
 document.addEventListener('keydown', function (event) {
-	this.fGravity();
-	if (event.code == 'KeyW' && this.mainObj.y >= 2) {
-		this.fBounce();
-    }
-	if (event.code == 'KeyS' && this.mainObj.y <= 460) {
-		//this.mainObj.y = this.mainObj.y + 3;
-	} 
-	if (event.code == 'KeyA' && this.mainObj.x >= 2) this.mainObj.x = this.mainObj.x - 3;
-	if (event.code == 'KeyD' && this.mainObj.x <= 466) this.mainObj.x = this.mainObj.x + 3;
+	let hero = DATA.mainHero;
+	let actions = DATA.actions;
+	let action = actions.filter(action => action.key === event.code);
+	if (action[0]){
+		hero.actions[action[0].name] = true;
+		console.log('action',action);
+	}
 }.bind(this));
 
+document.addEventListener('keyup', function (event) {
+	let hero = DATA.mainHero;
+	let actions = DATA.actions;
+	let action = actions.filter(action => action.key === event.code);
+	if (action[0]){
+		hero.actions[action[0].name] = false;
+		console.log('action',action);
+	}
+}.bind(this));
+
+function calcObjects(Obj){
+	var actions = Obj.actions;
+	if (actions.moveRight) {
+		if (checkMoveRight(Obj))
+		Obj.x = Obj.x + Obj.speed;
+	}
+	if (actions.moveLeft) {
+		if (checkMoveLeft(Obj))
+		Obj.x = Obj.x - Obj.speed;
+	}
+	if (!actions.jump) {
+		if (checkMoveDown(Obj))
+		Obj.y = Obj.y + DATA.gravity;
+	}
+	if (actions.moveUp) {
+		if (checkJump(Obj)) {
+			console.log('moveUp');
+			actions.jump = true;
+			actions.jumpTime = Obj.jumpHeight;
+		}
+		// if (checkMoveUp(Obj))
+		// Obj.y = Obj.y - DATA.gravity;
+	}
+	if (actions.jump) {
+		if (actions.jumpTime) {
+			actions.jumpTime = actions.jumpTime - 1
+			if (checkMoveUp(Obj)) {
+				Obj.y = Obj.y - Obj.jumpSpeed;
+			}
+			// console.log("Obj",Obj);
+		} else {
+			actions.jump = false;
+		}
+	}
+	// if (actions.moveLeft) {
+	// 	if (checkMoveLeft(Obj))
+	// 	Obj.x = Obj.x - Obj.speed;
+	// }
+	// return
+}
+function checkJump(Obj){
+	// console.log('check move up')
+	return !checkMoveDown(Obj)
+	// if (!checkMoveDown(Obj)){
+
+	
+}
+function checkMoveRight(Obj){ 
+	var aObs = DATA.roomOne.obstacles;
+	// x + width, y + width + height сравниваем с x, x+ height
+	//upRight = a, botRight = b;
+	var bResult =  aObs.every(function (oObs){
+		if (Obj.y + Obj.h > oObs.y && Obj.y < oObs.y + oObs.h) {// проверка по высоте
+			return Obj.x + Obj.w + Obj.speed < oObs.x || Obj.x + Obj.speed > oObs.x
+		} else return true
+	})
+	return bResult;
+}
+function checkMoveLeft(Obj){ 
+	var aObs = DATA.roomOne.obstacles;
+	// x + width, y + width + height сравниваем с x, x+ height
+	//upRight = a, botRight = b;
+	var bResult =  aObs.every(function (oObs){
+		if (Obj.y + Obj.h > oObs.y && Obj.y < oObs.y + oObs.h) {// проверка по y
+			// console.log("check left");
+			return Obj.x - Obj.speed > oObs.x + oObs.w || Obj.x - Obj.speed < oObs.x
+		} else return true
+	})
+	// console.log("check left",bResult,Obj );
+	return bResult;
+}
+function checkMoveUp(Obj) {
+	var aObs = DATA.roomOne.obstacles;
+	var bResult =  aObs.every(function (oObs){
+		if (Obj.x + Obj.w > oObs.x && Obj.x < oObs.x + oObs.w) {// проверка по x
+			console.log('chek up', Obj, oObs);
+			return Obj.y - Obj.jumpSpeed < oObs.y + oObs.h || Obj.y - Obj.jumpSpeed  > oObs.y + oObs.h
+		} else return true
+	})
+	return bResult;
+}
+function checkMoveDown(Obj){ 
+	var aObs = DATA.roomOne.obstacles;
+	var bResult =  aObs.every(function (oObs){
+		if (Obj.x + Obj.w > oObs.x && Obj.x < oObs.x + oObs.w) {// проверка по x
+			return Obj.y + Obj.h + DATA.gravity < oObs.y  || Obj.y > oObs.y + oObs.h
+		} else return true
+	})
+	return bResult;
+}
+//Попытка универсальной функции, но что то пошло не так
+// function checkMove(a1x,a1y,a2x,a2y,b1x,b1y,b2x,b2y, speed) {
+
+
+
+// 	if (Obj.y + Obj.h > oObs.y && Obj.y < oObs.y + oObs.h) {
+// 			return Obj.x + Obj.w + Obj.speed < oObs.x
+// 		} else return true
+// }
 
 document.addEventListener('mousedown', function (event) {
 
