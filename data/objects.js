172f88a420b071a25fec31f6fa85d42c2375841e
Nikolay <lgumoscow@gmail.com>
2021-10-27 15:55:42 +0300

    rework animation and add db

---
+++ data/objects.js
@@ -0,0 +1,18 @@
+var objectsDb = {
+	hero : {
+		w: 30,
+		h: 40,
+		jumpHeight: 18,
+		jumpSpeed: 10,
+		speed: 7,
+		anim: 
+			[ 
+				{name : "runR", src : "./img/sprite.png", frames: 8},
+				{name : "runL", src : "./img/sprite-revers.png", frames: 8},
+				{name : "idleR", src : "./img/sprite-costs.png", frames: 1},
+				{name : "idleL", src : "./img/sprite-costs-revers.png", frames: 1}
+			]
+	}
+}
+
+
